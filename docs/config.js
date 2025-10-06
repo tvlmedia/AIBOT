e5abed831dbde113fef6b073eef9d4862a3e27ec
tvlmedia <info@tvlmedia.nl>
2025-10-06 08:42:57 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // VUL HIER je backend-URL in (zonder trailing slash). Voorbeelden:
 // window.API_BASE = "https://aibot.fly.dev";
 // window.API_BASE = "https://aibot.onrender.com";
-window.API_BASE = "";
+window.API_BASE = "https://aibottvl.weathered-union-ddd4.workers.dev";
